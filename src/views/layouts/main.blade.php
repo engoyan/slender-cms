a25948eb794e93d05d1cfafdc7e0a98a5cdb67cc
Daniel Strøm <danielss89@gmail.com>
2013-02-21 10:39:34 +0100

    changed assets path in layout

--- src/views/layouts/main.blade.php
+++ src/views/layouts/main.blade.php
@@ -17,7 +17,7 @@
 
 
         <!-- Le styles -->
-        <link href="/packages/dws/slender-cms/assets/css/bootstrap.css" rel="stylesheet">
+        <link href="/css/bootstrap.css" rel="stylesheet">
         <style type="text/css">
           body {
             padding-top: 60px;
@@ -36,9 +36,9 @@
             }
           }
         </style>
-        <link href="/packages/dws/slender-cms/assets/css/bootstrap-responsive.css" rel="stylesheet">
-        <link href="/packages/dws/slender-cms/assets/css/bootstrap-select.min.css" rel="stylesheet">
-        <link href="/packages/dws/slender-cms/assets/css/style.css" rel="stylesheet">
+        <link href="/css/bootstrap-responsive.css" rel="stylesheet">
+        <link href="/css/bootstrap-select.min.css" rel="stylesheet">
+        <link href="/css/style.css" rel="stylesheet">
 
         @section('css')
         @show
@@ -49,15 +49,15 @@
 
         <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
         <!--[if lt IE 9]>
-          <script src="/packages/dws/slender-cms/assets/js/html5shiv.js"></script>
+          <script src="/js/html5shiv.js"></script>
         <![endif]-->
 
         <!-- Fav and touch icons -->
-        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/packages/dws/slender-cms/assets/ico/apple-touch-icon-144-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/packages/dws/slender-cms/assets/ico/apple-touch-icon-114-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/packages/dws/slender-cms/assets/ico/apple-touch-icon-72-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" href="/packages/dws/slender-cms/assets/ico/apple-touch-icon-57-precomposed.png">
-        <link rel="shortcut icon" href="/packages/dws/slender-cms/assets/ico/favicon.png">
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/ico/apple-touch-icon-144-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/ico/apple-touch-icon-114-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/ico/apple-touch-icon-72-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" href="/ico/apple-touch-icon-57-precomposed.png">
+        <link rel="shortcut icon" href="/ico/favicon.png">
     </head>
 
     <body>
@@ -113,9 +113,9 @@
 
         <!-- Javascripts
         ================================================== -->
-        <script src="/packages/dws/slender-cms/assets/js/jquery.v1.8.3.min.js"></script>
-        <script src="/packages/dws/slender-cms/assets/js/bootstrap.min.js"></script>
-        <script src="/packages/dws/slender-cms/assets/js/bootstrap-select.min.js"></script>
+        <script src="/js/jquery.v1.8.3.min.js"></script>
+        <script src="/js/bootstrap.min.js"></script>
+        <script src="/js/bootstrap-select.min.js"></script>
         @section('js')
         @show
     </body>
